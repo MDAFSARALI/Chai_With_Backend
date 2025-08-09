ff668ac7a6c94a20c32a85e9168e4dc6bec87101
MDAFSARALI <generaluserknown@gmail.com>
2025-08-09 18:24:26 +0530

    Connected To DB

---
+++ src/index.js
@@ -0,0 +1,43 @@
+// For 1St Approach  :
+/*
+import mongoose from "mongoose";
+import {DB_NAME} from './constants'
+
+*/
+// For 2nd Approach :
+//require('dotenv').config({path:'./env'})
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config({ path: "./env" });
+
+// FIRST APPROACH :
+// IIFE immediate invoked Function and semi colun is just for cleaning purpose
+
+/*
+import express from "express";
+const app=express();
+
+
+;(async ()=>{
+    try {
+        await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+        app.on("error" , (error)=>{
+            console.log("ERROR ",error)
+            throw error
+        })
+
+        app.listen(process.env.PORT,()=>{
+            console.log("App is listening on Port  "+process.env.PORT );
+        })
+    } catch (error) {
+        console.error("ERROR " +error)
+        throw error;
+    }
+})()
+
+*/
+
+//2ND APPROACH :
+
+connectDB();
