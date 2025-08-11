5676621c2d90170289143deda22f029534cdd85a
MDAFSARALI <generaluserknown@gmail.com>
2025-08-11 21:45:50 +0530

    User and Video model created

---
+++ src/models/user.model.js
@@ -0,0 +1,90 @@
+import mongoose, { model, Schema } from "mongoose";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt"
+const userSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+      index: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+      trim: true,
+    },
+    fullName: {
+      type: String,
+      required: true,
+      trim: true,
+      index: true,
+    },
+    avatar: {
+      type: String, //Cloudinary url
+      required: true,
+    },
+    coverimage: {
+      type: String, //Cloudinary url
+    },
+    watchhistory: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    password: {
+      type: String,
+      required: [true, "Password is required"],
+    },
+    refereshToken: {
+      type: String,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+// DON'T USE ARROW FUNCTION HERE : becuase we dont have this content in Arrow Function
+userSchema.pre("save",async function (next){
+    if(!this.isModified("password"))return next;
+    this.password=bcrypt.hash(this.password,10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect=async function(password){
+    return await bcrypt.compare(password , this.password);
+}
+
+userSchema.methods.generateAccesstoken=function (){
+    return jwt.sign(
+        {
+            _id:this._id,
+            email:this.email,
+            username:this.username,
+            fullName:this.fullName
+        },
+            process.env.ACCESS_TOKEN_SECRET,
+            {
+                expiresIn:process.env.ACCESS_TOKEN_EXPIRY
+            }
+    )
+}
+
+userSchema.methods.generateRefreshTken  =function (){
+    return jwt.sign(
+        {
+            _id:this._id,
+        },
+            process.env.REFRESH_TOKEN_SECRET,
+            {
+                expiresIn:process.env.REFRESH_TOKEN_EXPIRY
+            }
+    )
+}
+export const User = mongoose.model("User", userSchema);
