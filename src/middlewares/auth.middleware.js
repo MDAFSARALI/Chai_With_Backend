eae8a3dfc1badea51664fdd60075b5b3c33a3927
MDAFSARALI <generaluserknown@gmail.com>
2025-08-14 15:36:54 +0530

    Login and logout feature added

---
+++ src/middlewares/auth.middleware.js
@@ -0,0 +1,28 @@
+import { jwt } from "jsonwebtoken";
+import { ApiError } from "../utils/ApiError";
+import { asyncHandler } from "../utils/asyncHandler";
+import { User } from "../models/user.model";
+
+export const varifyJWT=asyncHandler (async (req,res,next)=>{
+    try {
+        const token=req.cookies?.accessToken || req.header("Authorization")?.replace("Bearer ","")
+        if(!token){
+            throw new ApiError(401,"Unauthorized Request")
+        }
+        const decodedToken= jwt.varify(token,process.env.ACCESS_TOKEN_SECRET)
+        const user=await User.findById(decodedToken?._id).select(
+            "-password -refreshToken"
+        )
+
+        if(!user){
+            throw new ApiError(401,"Invalid accessToken")
+        }
+        req.user=user;
+        next()
+    } catch (error) {
+        throw new ApiError(401,erro?.message || "Invalid access token")
+    }
+
+        
+
+})
